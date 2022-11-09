c58c212da08e2e30d8868b758b70d87ba71df6dd
hnaranjo <h.naranjo@utp.edu.co>
2022-11-08 22:51:07 -0500

    documents

---
+++ documents/documents.ts
@@ -0,0 +1,18 @@
+class Documents {
+
+    myName: String;
+    id: number;
+
+    function userName(id: number) {
+        if(id == 1) {
+            return myName = "Hector Naranjo";
+        }
+    
+    }
+}
+
+export Documents {
+    
+    console.log(userName(1));
+
+}
